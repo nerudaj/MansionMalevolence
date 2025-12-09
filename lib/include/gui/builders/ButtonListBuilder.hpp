404417b15f4161c7542ba949c569f8dfcb7ccd66
Jakub Neruda <nerudaj@email.cz>
2025-12-09 20:30:24 +0100

    Fix build

--- lib/include/gui/builders/ButtonListBuilder.hpp
+++ lib/include/gui/builders/ButtonListBuilder.hpp
@@ -23,8 +23,6 @@ public:
     ~ButtonListBuilder() = default;
 
 public:
-    ButtonListBuilder& addPadding(const std::string& percent);
-
     ButtonListBuilder& addButton(
         const StringId labelId,
         std::function<void(void)> onClick,
@@ -42,12 +40,7 @@ private:
         std::string buttonId;
     };
 
-    struct Padding
-    {
-        std::string heightPercent;
-    };
-
     const StringProvider& strings;
     const Sizer& sizer;
-    std::vector<std::variant<ButtonProps, Padding>> buttonProps;
+    std::vector<ButtonProps> buttonProps;
 };
--- lib/src/appstate/AppStateMainMenu.cpp
+++ lib/src/appstate/AppStateMainMenu.cpp
@@ -45,7 +45,6 @@ void AppStateMainMenu::buildLayout()
             .withTexturedTitle(
                 dic.resmgr.get<sf::Texture>("zombiecards_logo.png"))
             .withContent(ButtonListBuilder(dic.strings, dic.sizer)
-                             .addPadding("20%")
                              .addButton(StringId::PlayButton, [&] { onPlay(); })
                              .addButton(StringId::Options, [&] { onOptions(); })
                              .addButton(StringId::HowToPlay, [&] {})
