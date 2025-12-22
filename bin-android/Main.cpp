051ff8fce82e1c4f1d49052356cb22ad83d98ebf
Jakub Neruda <nerudaj@email.cz>
2025-12-22 20:17:34 +0100

    Update Main.cpp

--- bin-android/Main.cpp
+++ bin-android/Main.cpp
@@ -44,11 +44,6 @@ int main(int, char*[])
         auto&& app = dgm::App(window);
         auto&& dependencies = DependencyContainer(window, "", Language::English, settings);
 
-        settings.audio.registerObserver([&dependencies](const AudioSettings& settings)
-        {
-            dependencies.jukebox.setVolume(settings.musicVolume);
-        });
-
         window.getSfmlWindowContext().setMouseCursorVisible(false);
 
         app.pushState<AppStateMainMenu>(dependencies, dependencies.settings);
