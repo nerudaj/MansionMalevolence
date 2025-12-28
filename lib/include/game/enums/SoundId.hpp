5767e50b23a18e3857388f340ed89ea25d7d7679
Jakub Neruda <nerudaj@email.cz>
2025-12-28 21:58:20 +0100

    Minor sound updates

--- lib/include/game/enums/SoundId.hpp
+++ lib/include/game/enums/SoundId.hpp
@@ -17,9 +17,9 @@ namespace SoundId
     const IdType WeaponReload = "shotgun_reload.wav";
     const IdType ShotgunFire = "SHOT01.wav";
     const IdType PistolFire = "GUN01.wav";
-    const IdType SilencedPistolFire = "GUN01_muffled.wav";
+    const IdType SilencedPistolFire = "zombie_cards_silenced_pistol.wav";
     const IdType Crossbow = "crossbow.wav";
-    const IdType LauncherFire = "launcher_fire.wav";
+    const IdType LauncherFire = "zombie_cards_launcher.wav";
     const IdType CrateOpen = "crate.wav";
     const IdType DoorUnlock = "KEY_DOOR.wav";
     const IdType Heal = "heal.wav";
